55ad69f6c80e88e4ae775830c1c243b9b2c5d9e4
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-02-03 21:19:36 -0800

    eh who cares I'll just stop hydrating
    Fixes #3

--- script/build.ts
+++ script/build.ts
@@ -1,4 +1,3 @@
-import { exec } from 'child_process'
 import esbuild from 'esbuild'
 import fs from 'fs/promises'
 
@@ -12,10 +11,7 @@ const context = await esbuild.context({
   outdir: devMode ? 'static/' : 'dist/',
   sourcemap: devMode,
   minify: !devMode,
-  supported: { nesting: !devMode },
-  define: {
-    DEV_MODE: String(devMode)
-  }
+  supported: { nesting: !devMode }
 })
 
 if (devMode) {
@@ -25,43 +21,7 @@ if (devMode) {
 } else {
   await context.rebuild()
   await context.dispose()
-
   await fs.mkdir('dist/peer/', { recursive: true })
-
-  for (const [entry, cssPath] of Object.entries({
-    'script/build-qr.tsx': 'dist/index.css',
-    'script/build-peer.tsx': 'dist/index-peer.css'
-  })) {
-    await esbuild.build({
-      entryPoints: [entry],
-      bundle: true,
-      outfile: 'script/build.js',
-      platform: 'node',
-      packages: 'external',
-      format: 'esm',
-      minify: true
-    })
-    // @ts-ignore Won't exist until built
-    await import(`./build.js?_=${entry}`)
-    // CSS module class name mangling might not be guaranteed to be consistent
-    const { stdout, stderr } = await new Promise<{
-      stdout: string
-      stderr: string
-    }>((resolve, reject) => {
-      exec(`diff script/build.css ${cssPath}`, (err, stdout, stderr) => {
-        if (err && err.code !== 1) {
-          reject(err)
-          return
-        }
-        resolve({ stdout, stderr })
-      })
-    })
-    if (stdout || stderr) {
-      console.warn(`[${entry}]`)
-      console.warn('[[stdout]]')
-      console.warn(stdout)
-      console.warn('[[stderr]]')
-      console.warn(stderr)
-    }
-  }
+  await fs.copyFile('static/index.html', 'dist/index.html')
+  await fs.copyFile('static/peer/index.html', 'dist/peer/index.html')
 }
--- src/render.ts
+++ src/render.ts
@@ -1,18 +1,10 @@
 import { ReactNode } from 'react'
-import { createRoot, hydrateRoot } from 'react-dom/client'
-
-// Avoid bundling `hydrateRoot` when built (though React doesn't seem to be tree
-// shakeable so this currently doesn't do anything)
-declare const DEV_MODE: boolean
+import { createRoot } from 'react-dom/client'
 
 export function render (app: ReactNode): void {
   const root = document.getElementById('root')
   if (!root) {
     throw new TypeError('#root not found')
   }
-  if (DEV_MODE) {
-    createRoot(root).render(app)
-  } else {
-    hydrateRoot(root, app)
-  }
+  createRoot(root).render(app)
 }
